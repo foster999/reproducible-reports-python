f1508e50426a1510041074ded7b5d8537a08bfa4
foster999 <foster.dev999@gmail.com>
2021-08-30 17:35:28 +0100

    Remove unused sphinx config options

--- conf.py
+++ conf.py
@@ -31,9 +31,6 @@ extensions = [
     "myst_nb"
     ]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
@@ -48,11 +45,6 @@ master_doc = "example"
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
 
 # Remove the sidebar, as we're only building a single page
 html_theme_options = {'nosidebar': True}
